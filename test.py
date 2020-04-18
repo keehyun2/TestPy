0f1c4138e40374749d1b79d0c37d08394c758a19
박기현 <keehyun2@gmail.com>
2020-04-18 13:55:04 +0900

    xxx

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import sys
 import html
-# 아래 명령어로 설치 먼저 해야함. 
-# pip3 install selenium BeautifulSoup
+# 필요한 파이썬 라이브러리 
+# # pip3 install selenium beautifulsoup4
+
+# chromedriver 는 설치된 크롬에 호환되는 버전을 받아서 사용할것
+# 크롬 드라이버 다운로드 - https://chromedriver.chromium.org/downloads
+# 크롬버전 확인 - chrome://settings/help
 
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
@@ -30,3 +34,4 @@ for article in article_list:
     pass
 
 driver.quit()
+
